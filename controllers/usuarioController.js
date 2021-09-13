4496dabfe31b91cd9182c33064aea585b194bdd0
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-13 19:18:57 -0300

    Insertar Usuarios en la base de datos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,8 +1,14 @@
 const Usuario = require('../models/Usuario')
 
 exports.nuevoUsuario = async (req,res)=>{
-    console.log(req.body)
-    const usuario =await new Usuario(req.body);
+    //verificar si el usuario ya esta registrado
+    const { email }=req.body;
+    let usuario = await Usuario.findOne({email});
+    if(usuario){
+        return res.status(400).json({msg: 'El usuariuo ya esta registrado'})
+    }
+
+    usuario = await new Usuario(req.body);
     usuario.save()
     res.json({msg: 'Usuario creado correctamente'})
 }
