d8920ae3b1590ce1b88c16e74b3c7758c172b27f
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-13 19:15:01 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const conectarDB = require('./config/db')
+
+//crear el servidor
+const app = express();
+
+console.log(`comenzando NodeSend`);
+
+//conectar a la base de datos
+conectarDB()
+
+//p[uerto de la app
+const port = process.env.PORT || 4000;
+
+//habilitar los valores de un body
+
+app.use(express.json())
+
+//rutas de la app
+app.use('/api/usuarios',require('./routes/usuarios'));
+
+
+//arrancar la app
+
+app.listen(port, '0.0.0.0',()=>{
+    console.log(`servidor funcionando en el puerto ${port}`)
+})
+
