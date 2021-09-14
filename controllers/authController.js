3dad95eb603e6c773a257db51859d0ce408a66fc
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-13 21:32:53 -0300

    Update authController.js
    Obtener el Usuario Autenticado por el JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,64 @@
-const Usuario = require ('../models/Usuario')
-const bcrypt= require ('bcrypt')
-const jwt= require('jsonwebtoken')
-require ('dotenv').config({path:'variables.env'})
-const {validationResult}=  require('express-validator')
+const Usuario = require("../models/Usuario");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
+const { validationResult } = require("express-validator");
 
-exports.autenticarUsuario = async (req,res,next)=>{
-    //revisar errores
-      //mensajes de error de express validator 
-      const errores= validationResult(req);
-      if(!errores.isEmpty()){
-          return res.status(400).json({errores: errores.array()})
+exports.autenticarUsuario = async (req, res, next) => {
+  //revisar errores
+  //mensajes de error de express validator
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  //buscar el usuario
+  const { email, password } = req.body;
+  const usuario = await Usuario.findOne({ email });
+  console.log(usuario);
+  if (!usuario) {
+    res.status(401).json({ msg: "El usuario no existe" });
+    return next();
+  }
+
+  //verificar el password y autenticar el usuario
+  if (bcrypt.compareSync(password, usuario.password)) {
+    console.log("el password es correcto");
+    //crear jsonwebtoken
+    const token = jwt.sign(
+      {
+        nombre: usuario.nombre,
+        id: usuario._id,
+        email: usuario.email,
+      },
+      process.env.SECRETA,
+      {
+        expiresIn: "8h",
       }
-  
-    //buscar el usuario
-    const {email, password}= req.body
-    const usuario = await Usuario.findOne({email})
-    console.log(usuario)
-    if(!usuario){
-        res.status(401).json({msg: 'El usuario no existe'})
-        return next();
-    }
+    );
+    res.json({ token });
+  } else {
+    res.status(401).json({ msg: "Password Incorrecto" });
+    return next();
+  }
+};
 
-   
-    //verificar el password y autenticar el usuario
-    if(bcrypt.compareSync(password, usuario.password)){
-        console.log('el password es correcto')
-        //crear jsonwebtoken
-        const token = jwt.sign({
-            nombre:usuario.nombre,
-            id:usuario._id,
-            email:usuario.email
-        },process.env.SECRETA, {
-            expiresIn:'8h'
-        });
-        res.json({token});
-    }
-    else{
-        res.status(401).json({msg:"Password Incorrecto"});
-        return next();
-    }
-}
+exports.usuarioAutenticado = async (req, res, next) => {
+  const authHeader = req.get("Authorization");
+  if (authHeader) {
+    //obtener el token
+    const token = authHeader.split(" ")[1];
 
-exports.usuarioAutenticado = async (req,res)=>{
+    //comprobar el jsonwebtoken
+    try {
+      const usuario = jwt.verify(token, process.env.SECRETA);
 
-}
+      res.json({ usuario });
+    } catch (error) {
+      console.log(error);
+      console.log(`jwt no valido`);
+    }
+  }
+
+  return next();
+};
