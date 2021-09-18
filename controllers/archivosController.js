cb1dc16d86cabc1da00b82fc4f4d181fac2e879b
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-18 15:23:16 -0300

    Creando Routing y Controllers para Archivos

---
+++ controllers/archivosController.js
@@ -0,0 +1,7 @@
+exports.subirArchivo = async (req,res)=>{
+    console.log(`desde subir archivo`)
+}
+
+exports.eliminarArchivo= async (req,res)=>{
+    console.log(`desde eliminar archivo`)
+}
--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(express.json())
 app.use('/api/usuarios',require('./routes/usuarios'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/enlaces',require('./routes/enlaces'));
+app.use('/api/archivos',require('./routes/archivos'));
 
 
 //arrancar la app
